b26b239ecd8de2e1dba0b165a19230c94dd70e99
aldairandre <aldairandre99@gmail.com>
2023-03-27 22:15:33 +0100

    🚀 Starting

---
+++ spreader-bot/bin/spreader_bot.dart
@@ -0,0 +1,5 @@
+import 'package:spreader_bot/spreader_bot.dart' as spreader_bot;
+
+void main(List<String> arguments) {
+  print('Hello world: ${spreader_bot.calculate()}!');
+}
---
+++ spreader-bot/test/spreader_bot_test.dart
@@ -0,0 +1,8 @@
+import 'package:spreader_bot/spreader_bot.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
